bd3140fa0bf5e2371933c08f130a31139c341221
Daniel Rangel Figueroa <drangelfigueroa@csumb.edu>
2021-11-25 19:42:41 -0800

    added remove button

--- app.py
+++ app.py
@@ -1,28 +1,32 @@
-from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
-from flask_wtf import file
+from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 from flask_uploads import UploadSet, IMAGES, configure_uploads
 import forms
 import os
-global filename
 
+global img_name
+
+img_name = None
 SECRET_KEY = os.urandom(32)
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
-bootrap = Bootstrap(app)
+# bootrap = Bootstrap(app)
 
 app.config['UPLOADED_IMAGES_DEST'] = '/static/uploads/'
 images = UploadSet('images', IMAGES)
 configure_uploads(app, images)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    global filename
-    filename = None
+    global img_name
     form = forms.UploadImage()
+    # Gets file name and saves file
     if form.validate_on_submit():
-        filename = secure_filename(form.img.data.filename)
-        form.img.data.save('static/uploads/' + filename)
-    return render_template('index.html', form=form, img=filename)
+        img_name = secure_filename(form.img.data.filename)
+        form.img.data.save('static/uploads/' + img_name)
+    # Removes image
+    if 'rm' in request.form:
+        os.remove(f'static/uploads/{img_name}')
+        img_name = None
+    return render_template('index.html', form=form, img=img_name)
