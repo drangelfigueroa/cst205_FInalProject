3231e8bf36905ace4dd8fddfb4518caae43f127a
Daniel Rangel Figueroa <drangelfigueroa@csumb.edu>
2021-12-01 17:54:17 -0800

    file name fix

--- app.py
+++ app.py
@@ -3,14 +3,11 @@ from werkzeug.utils import secure_filename
 from flask_uploads import UploadSet, IMAGES, configure_uploads
 import forms
 import os
+import manipulations as man
 
-global img_name
-
-img_name = None
 SECRET_KEY = os.urandom(32)
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
-# bootrap = Bootstrap(app)
 
 app.config['UPLOADED_IMAGES_DEST'] = '/static/uploads/'
 images = UploadSet('images', IMAGES)
@@ -19,14 +16,16 @@ configure_uploads(app, images)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    global img_name
     form = forms.UploadImage()
     # Gets file name and saves file
     if form.validate_on_submit():
-        img_name = secure_filename(form.img.data.filename)
-        form.img.data.save('static/uploads/' + img_name)
+        man.img_name = secure_filename(form.img.data.filename)
+        form.img.data.save('static/uploads/' + man.img_name)
+
+        ## test function in manipulations.py
+        man.test()
     # Removes image
     if 'rm' in request.form:
-        os.remove(f'static/uploads/{img_name}')
-        img_name = None
-    return render_template('index.html', form=form, img=img_name)
+        os.remove(f'static/uploads/{man.img_name}')
+        man.img_name = None
+    return render_template('index.html', form=form, img=man.img_name)
--- manipulations.py
+++ manipulations.py
@@ -1,1 +1,19 @@
-## All of our image manipulation functions should go here.
+"""
+ All of our image manipulation functions should go here.
+
+ img_name: name of unedited image.
+ edit_name: name of edited image.
+"""
+global img_name, edit_name
+img_name = None
+
+
+def test():
+    # calls global variables
+    global img_name, edited_name
+
+    # How edited image should be saved as in static/upluads
+    edited_name = f'edited_{img_name}'
+
+    # Prints in console
+    print(f'Unedited: {img_name}\nedited: {edited_name}')
