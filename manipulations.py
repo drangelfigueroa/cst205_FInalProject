d2e5853b64e0f07bdb5bc9a7a9dc322fc6c1e312
Roshan2020 <61339429+Roshan2020@users.noreply.github.com>
2021-12-12 22:02:07 -0800

    Update manipulations.py

--- manipulations.py
+++ manipulations.py
@@ -1,3 +1,10 @@
+# CST207 - Final Project
+# Time: Fall 2021
+# Date: 13-Dec.-2021
+# School: CSUMB
+# Author: Team 795
+# Description: Application to convert IMG to an edited version based on user's choosing
+#########################################################################################
 """
  All of our image manipulation functions should go here.
 
@@ -7,6 +14,7 @@
 from PIL import Image, ImageFilter, ImageOps
 import cv2
 import os
+import pytesseract
 
 global img_name, edited_name
 img_name = None
@@ -19,9 +27,12 @@ edited_name = None
 # Format: (String for dropdown, function name)
 MANIPULATIONS = [('Black & White', 'bnw'), ('Blur', 'blur'), ('Contour', 'contour'), ('Edge Enhance', 'edge_enhance'),
                 ('Emboss', 'emboss'), ('Face Detect', 'faces'), ('Find Edges','find_edges'), ('Mirror Flip', 'hRef'), 
-                ('Scale Up', 'upScale'), ('Scale Down', 'downScale'), ('Sepia', 'sepia'), ('Smooth', 'smooth')]
+                ('Scale Up', 'upScale'), ('Scale Down', 'downScale'), ('Sepia', 'sepia'), ('Smooth', 'smooth'),
+                ('Negative', 'neg'), ('Grey Scale', 'greyScale')]
 
-
+####################################################
+# List of Functions
+####################################################
 def call_function(selected):
     """
         Will call function by itself. No need to add anything
@@ -31,7 +42,10 @@ def call_function(selected):
         if(name[0] == selected):
             globals()[name[1]]()
 
-
+########################################################
+# Function
+# Action: Formula for detecting the faces in IMG
+########################################################
 def faces():
     """ Face Detection algorithm """
     global img_name, edited_name
@@ -53,7 +67,11 @@ def faces():
     edited_name = f'edited_{img_name}'
     cv2.imwrite(f'static/uploads/{edited_name}', img)
 
-
+########################################################
+# Sub-function
+# Action: Formula for Every pixel change into Sepia
+# Location: Sepia Function
+########################################################
 def sepia_getpixel(pixel):
     if pixel[0] < 63:
         r, g, b = int(pixel[0]*1.1), pixel[1], int(pixel[2]*.9)
@@ -66,7 +84,10 @@ def sepia_getpixel(pixel):
         g, b = pixel[1], pixel[2]//2
     return r, g, b
 
-
+########################################################
+# Function
+# Action: Formula for scaling down the IMG
+########################################################
 def downScale():
     """ scale down image """
     global img_name, edited_name
@@ -80,7 +101,10 @@ def downScale():
     edited_name = f'edited_{img_name}'
     im.save(f'static/uploads/{edited_name}')
 
-
+########################################################
+# Function
+# Action: Formula for scaling up the IMG
+########################################################
 def upScale():
     """ scale up image """
     global img_name, edited_name
@@ -93,7 +117,10 @@ def upScale():
     edited_name = f'edited_{img_name}'
     im.save(f'static/uploads/{edited_name}')
 
-
+########################################################
+# Function
+# Action: Formula for converting IMG to Black & White
+########################################################
 def bnw():
     """ converts to black and white """
     global img_name, edited_name
@@ -103,7 +130,10 @@ def bnw():
     edited_name = f'edited_{img_name}'
     im.save(f'static/uploads/{edited_name}')
 
-
+########################################################
+# Function
+# Action: Formula to flipping IMG over the X-axis
+########################################################
 def hRef():
     """ Reflects the image on the 'X' axis """
     global img_name, edited_name
@@ -113,7 +143,10 @@ def hRef():
     edited_name = f'edited_{img_name}'
     im.save(f'static/uploads/{edited_name}')
 
-
+########################################################
+# Function
+# Action: Formula to edit IMG with Sepia
+########################################################
 def sepia():
     global img_name, edited_name
 
@@ -125,7 +158,10 @@ def sepia():
     edited_name = f'edited_{img_name}'
     im.save(f'static/uploads/{edited_name}')
 
-
+########################################################
+# Function
+# Action: Formula to clear IMG
+########################################################
 def rm_img():
     """Helper function to clear image"""
     global img_name
@@ -133,7 +169,10 @@ def rm_img():
         os.remove(f'static/uploads/{img_name}')
         img_name = None
 
-
+########################################################
+# Function
+# Action: Formula to clear editied IMG
+########################################################
 def rm_edited():
     """Helper function to clear edited image"""
     global edited_name
@@ -141,7 +180,10 @@ def rm_edited():
         os.remove(f'static/uploads/{edited_name}')
         edited_name = None
 
-
+########################################################
+# Function
+# Action: Formula to blur the IMG
+########################################################
 def blur():
     global img_name, edited_name
 
@@ -151,7 +193,10 @@ def blur():
     edited_name = f'edited_{img_name}'
     im.save(f'static/uploads/{edited_name}')
 
-
+########################################################
+# Function
+# Action: Formula to contour the IMG
+########################################################
 def contour():
     global img_name, edited_name
 
@@ -161,7 +206,10 @@ def contour():
     edited_name = f'edited_{img_name}'
     im.save(f'static/uploads/{edited_name}')
 
-
+########################################################
+# Function
+# Action: Formula to emboss the IMG
+########################################################
 def emboss():
     global img_name, edited_name
 
@@ -171,7 +219,10 @@ def emboss():
     edited_name = f'edited_{img_name}'
     im.save(f'static/uploads/{edited_name}')
 
-
+########################################################
+# Function
+# Action: Formula to find edges the IMG
+########################################################
 def find_edges():
     global img_name, edited_name
 
@@ -181,7 +232,10 @@ def find_edges():
     edited_name = f'edited_{img_name}'
     im.save(f'static/uploads/{edited_name}')
 
-
+########################################################
+# Function
+# Action: Formula to enhance edges the IMG
+########################################################
 def edge_enhance():
     global img_name, edited_name
 
@@ -191,7 +245,10 @@ def edge_enhance():
     edited_name = f'edited_{img_name}'
     im.save(f'static/uploads/{edited_name}')
 
-
+########################################################
+# Function
+# Action: Formula to smoothen the IMG
+########################################################
 def smooth():
     global img_name, edited_name
 
@@ -200,3 +257,30 @@ def smooth():
 
     edited_name = f'edited_{img_name}'
     im.save(f'static/uploads/{edited_name}')
+
+####################################################
+# Function
+# Action: Edit IMG into GrayScale
+####################################################
+def greyScale():
+    global img_name, edited_name
+    print("Okay")
+    img = cv2.imread(f'static/uploads/{img_name}', cv2.IMREAD_GRAYSCALE)
+
+    edited_name = f'edited_{img_name}'
+    cv2.imwrite(f'static/uploads/{edited_name}', img)
+
+####################################################
+# Function
+# Action: Edit IMG with Negative filter
+####################################################
+def neg():
+    global img_name, edited_name
+    img = cv2.imread(f'static/uploads/{img_name}')
+
+    img_not = cv2.bitwise_not(img)
+
+    edited_name = f'edited_{img_name}'
+    cv2.imwrite(f'static/uploads/{edited_name}', img_not)
+
+# END OF CODE
